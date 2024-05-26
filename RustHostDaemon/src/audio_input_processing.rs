2b072c15124f7892b00b6dea41fe48d641e186a0
William Redenbaugh <wmredenbaugh@gmail.com>
2024-05-26 05:04:07 -0700

    Multithreaded audio processing pipeline

--- RustHostDaemon/src/audio_input_processing.rs
+++ RustHostDaemon/src/audio_input_processing.rs
@@ -2,23 +2,40 @@
 use std::thread;
 use std::thread::sleep;
 use std::time::Duration;
+use cpal::SampleRate;
 use rustfft::{FftPlanner, num_complex::Complex};
 use std::sync::mpsc::{self, Sender, Receiver};
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use cpal::{traits::{DeviceTrait, HostTrait, StreamTrait}, SupportedStreamConfig, SupportedStreamConfigRange};
 
-fn fft_processing_thread(rx: Receiver<Vec<i16>>, tx_complex_data: Sender<Vec<Complex<i16>>>){
 
-    
+fn fft_processing_thread(rx: Receiver<Vec<i16>>, tx_complex_data: Sender<Vec<Complex<f32>>>){
     loop{
-        let data = rx.recv().unwrap();
-
-        let mut planner: FftPlanner<i16> = FftPlanner::new();
-        let fft = planner.plan_fft_forward(data.len());
+        let mut data = rx.recv().unwrap();
+        for l in 0..512{
+            data[l] = data[l]/2;   
+        }
+        data.truncate(512);
+        let mut planner: FftPlanner<f32> = FftPlanner::new();
+        let mut fft = planner.plan_fft_forward(data.len());
         
-        let mut complex_data: Vec<Complex<i16>> = data.into_iter().map(|x| Complex::new(x, 0)).collect();
-
+        let mut complex_data: Vec<Complex<f32>> = data.into_iter().map(|x| Complex::new(x as f32, x as f32)).collect();
         fft.process(&mut complex_data);
+
         tx_complex_data.send(complex_data).unwrap();
+    }
+}
+
+fn fft_inverse_processing_thread(tx_complex_data: Sender<Vec<Complex<f32>>>, incoming_unfiltered_fft_data: Receiver<Vec<Complex<f32>>>){
+    loop{
+
+        let mut incoming_data = incoming_unfiltered_fft_data.recv().unwrap();
+        // Apply inverse FFT to get the smoothed data
+        let mut ifft_output = vec![Complex::new(0.0, 0.0); 512];
+        let mut planner = FftPlanner::new();
+        let ifft = planner.plan_fft(512, rustfft::FftDirection::Inverse);
+
+        ifft.process_with_scratch(&mut incoming_data, &mut ifft_output);
+        tx_complex_data.send(ifft_output);
     }
 }
 
@@ -28,42 +45,54 @@ fn microphone_input_thread(tx: Sender<Vec<i16>>){
     let host = cpal::default_host();
     let device = host.default_input_device().expect("failed to find input device");
 
-    let config = device
+    let config_range = SupportedStreamConfigRange::new(1, 
+        SampleRate(17200), 
+        SampleRate(17200), 
+        cpal::SupportedBufferSize::Range {
+            min: (512), 
+            max: (1024)} , 
+            cpal::SampleFormat::I16);
+    let config = config_range.with_max_sample_rate();
+    
+    device
     .default_input_config()
     .expect("Failed to get default input config");
     println!("Default input config: {:?}", config);
+    // Wrap the Duration in an Option
+    let tx_copy = tx.clone();
+    // Create and start the input stream.
+    let stream = device.build_input_stream(
+        &config.config(),
+        move |data: &[i16], _: &cpal::InputCallbackInfo| {
+            //println!("Length: {}", data.len());
+            if(data.len() < 512){
+                return;
+            }
+            tx_copy.send(data.to_vec()).unwrap();
+        },
+        move |err| {
+            eprintln!("An error occurred on the input audio stream: {}", err);
+        }, 
+        None
+    ).expect("Failed to build input stream");
+
+    stream.play().unwrap();  
 
     loop{
-        let duration = Duration::from_millis(10);
-
-        // Wrap the Duration in an Option
-        let tx_copy = tx.clone();
-        // Create and start the input stream.
-        let stream = device.build_input_stream(
-            &config.config(),
-            move |data: &[i16], _: &cpal::InputCallbackInfo| {
-                println!("Length: {}", data.len());
-                tx_copy.send(data.to_vec()).unwrap();
-            },
-            move |err| {
-                eprintln!("An error occurred on the input audio stream: {}", err);
-            }, 
-            Some(duration)
-        ).expect("Failed to build input stream");
-
-        stream.play().unwrap();        
-        sleep(duration);
+        sleep(Duration::from_secs(5));
     }
 }
 
-pub fn initialize_audio_pipeline() -> Receiver<Vec<Complex<i16>>>{
+pub fn initialize_audio_pipeline() -> Receiver<Vec<Complex<f32>>>{
     // Create pipes
     let (tx_complex_data, rx_complex_data) = mpsc::channel();
     let (tx, rx): (Sender<Vec<i16>>, Receiver<Vec<i16>>) = mpsc::channel();
     
+    let (tx_normalized_data, rx_nromalized_data) = mpsc::channel();
     // Spawn thread for processing the FFT data
     
+    thread::spawn(move || fft_inverse_processing_thread(tx_normalized_data, rx_complex_data));
     thread::spawn(move || fft_processing_thread(rx, tx_complex_data));
     thread::spawn(move || microphone_input_thread(tx));
-    return rx_complex_data;
+    return rx_nromalized_data;
 }
--- RustHostDaemon/src/main.rs
+++ RustHostDaemon/src/main.rs
@@ -3,6 +3,9 @@ mod audio_input_processing;
 use std::thread::sleep;
 use std::time::Duration;
 
+use rustfft::num_complex::ComplexFloat;
+use rustfft::num_traits::abs;
+
 
 
 fn main() {
@@ -12,20 +15,40 @@ fn main() {
     udp_matrix.set_pixel(0, 0, 100, 100, 100);
     udp_matrix.update();
     
-    //let audio_data = audio_input_processing::initialize_audio_pipeline();
-
-    let mut val:u32 = 0;
+    let audio_data = audio_input_processing::initialize_audio_pipeline();
+    let mut vals: [u8; 16] = [0; 16];
     loop{
-        for x in 0..16{
-            for y in 0..16{
-                val = val + 20;
-                val = val % 255;
 
-                udp_matrix.set_pixel_hsv(x, y, val as u8, 255, 255)
+        let data = audio_data.recv().unwrap();
+        for x in 0..16{
+
+            let mut val = 0; 
+            for j in 0..16{
+                let mut sub_val;
+                unsafe{
+                    sub_val = (data[x * 16 + j].re).abs() as i32;
+                }
+                sub_val = sub_val/30000;
+                val = (val + sub_val)/2;
+            }
+
+            if val > 16{
+                val = 16;
+            }
+            
+            if(val > vals[x] as i32){
+                vals[x] = val as u8;
+            }
+            for y in 0..vals[x]{
+                udp_matrix.set_pixel_hsv(x, y as  usize, 255 - vals[x] * 6, 255, 255)
+            }
+            for y in vals[x]..16{
+                udp_matrix.set_pixel(x, y as usize, 0, 0, 0);
+            }
+            if(vals[x] > 0){
+                vals[x] = vals[x] - 1;
             }
         }
         udp_matrix.update();
-        println!("updating...");
-        sleep(Duration::from_millis(10));
     }
 }
